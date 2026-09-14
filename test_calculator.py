92a1c6a3c6918c532f1998da14cbae4e4ac7934a
Pavan <netrakrpavan@gmail.com>
2026-09-14 20:03:26 +0530

    Added calculator tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,13 @@
-from calculator import add
+from calculator import add, subtract, multiply
+
 
 def test_add():
     assert add(10, 20) == 30
+
+
+def test_subtract():
+    assert subtract(20, 10) == 10
+
+
+def test_multiply():
+    assert multiply(10, 20) == 200
